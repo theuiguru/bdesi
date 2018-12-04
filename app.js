8521bfaf5a59cf4bf660ec393c16c76bd2c8f295
Chris Thomas <webmaster102@gmail.com>
2018-12-03 23:33:48 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        var lat = position.coords.latitude,
+            lon = position.coords.longitude;
+        console.log('Geolocation - Latitude: '+ lat +', Longitude: '+ lon);
+    });
+}
+else {
+    console.log('Geolocation is not supported for this Browser/OS version yet.');
+}
