290524b9f03bd82fa99c10bc70791f7dc377524c
Chris Thomas <webmaster102@gmail.com>
2022-11-23 13:19:37 -0500

    button click to geolocate
    added fontawesome icon

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const year = document.querySelector('.year').textContent = new Date().getFullYear();
-
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        var lat = position.coords.latitude,
-            lon = position.coords.longitude;
-        console.log('Geolocation - Latitude: '+ lat +', Longitude: '+ lon);
+const bdesi = document.querySelector('.bdesi');
+bdesi.addEventListener('click', geolocate);
+function geolocate() {
+    if('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition((position) => {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        console.log('Latitude: ' + lat + ', Longitiude: ' + lon);
     });
-}
-else {
-    console.log('Geolocation is not supported for this Browser/OS version yet.');
+    } else {
+    console.log('Geolocation not available.');
+    }
 }
