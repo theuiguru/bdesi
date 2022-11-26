1ac5299e72d5e624934630c5ff814450ce5ca1ac
Chris Thomas <webmaster102@gmail.com>
2022-11-26 10:19:27 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const year = document.querySelector('.year').textContent = new Date().getFullYea
 const bdesi = document.querySelector('.bdesi');
 bdesi.addEventListener('click', geolocate);
 function geolocate() {
-    if('geolocation' in navigator) {
-        navigator.geolocation.getCurrentPosition((position) => {
-          const lat = position.coords.latitude;
-          const lon = position.coords.longitude;
-          console.log('Latitude: ' + lat + ', Longitiude: ' + lon);
-        });
-    } else {
-        console.log('Geolocation not available.');
-    }
+  if('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+      console.log('Latitude: ' + lat + ', Longitiude: ' + lon);
+    });
+  } else {
+    console.log('Geolocation not available.');
+  }
 }
